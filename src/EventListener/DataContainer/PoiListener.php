acfd7c3ea3ffef76f093a63caa58c7533f3cd6aa
eki89 <fabian@oveleon.de>
2023-05-22 12:09:42 +0200

    [Bugfix] Decode html messages for backoffice.

--- src/EventListener/DataContainer/PoiListener.php
+++ src/EventListener/DataContainer/PoiListener.php
@@ -62,7 +62,7 @@ class PoiListener
         {
             $output[] = vsprintf("%s\n%s", [
                 date(Config::get("datimFormat"), $message['tstamp']),
-                strip_tags(str_replace(['<br>', '<br/>', '<br />'], "\n", $message['message']))
+                strip_tags(str_replace(['<br>', '<br/>', '<br />'], "\n", html_entity_decode($message['message'])))
             ]);
         }
 
